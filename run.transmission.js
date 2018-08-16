48e6878ae6066105c6bf470ea4da1e4c6c592716
pingpouff <pingpouff@gmail.com>
2018-08-16 22:35:43 +0200

    added promise to transmission api

--- run.transmission.js
+++ run.transmission.js
@@ -2,15 +2,15 @@ var tvmaze = require("./tvmaze.api")();
 var zooqle = require("./zooqle.api")();
 var moment = require("moment");
 var promise = require("bluebird");
-var Transmission = promise.promisifyAll(require("transmission"));
-var transmission = new Transmission({
+var Transmission = require("transmission");
+var transmission = promise.promisifyAll(new Transmission({
   host: "localhost", // default 'localhost'
   port: 9091, // default 9091
   username: "", // default blank
   password: "", // default blank
   ssl: false, // default false use https
   url: "/transmission/rpc" // default '/transmission/rpc'
-});
+}));
 // var transPro = promise.promisifyAll(transmission);
 var series = require("./shows");
 
@@ -22,11 +22,8 @@ var download = function(name) {
     .then(
       zooqle.execIf(tvmaze.episodeAiredAfter(start), ep =>
         zooqle.getOne(ep).then(tor =>
-          transmission.addUrl(tor.magnet, (err, result) => {
-            if (err) {
-              return console.log(err);
-            }
-            var id = result.id;
+          transmission.addUrlAsync(tor.magnet).then(result => {
+            var id = result.id; 
             console.log("Just added a new torrent.");
             console.log("Torrent ID: " + id);
           })
